1379b16a94ea4be9afd17abe8852c5d0c010399c
33-66 <simon.nganga@student.moringaschool.com>
2023-10-20 11:51:44 +0300

    updated work

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = "bob"
+function upperCaseCustomerName() {
+    if (typeof window.customerName === 'string') {
+        window.customerName = window.customerName.toUpperCase();
+    } else {
+        ('customerName is not a valid string.');
+    }
+}
+function setBestCustomer(){
+    window.bestCustomer ='not bob';
+}
+function overwriteBestCustomer(){
+    window.bestCustomer = 'maybe bob'
+}
+function changeLeastFavoriteCustomer() {
+    const leastFavoriteCustomer = 'mata';
+    leastFavoriteCustomer = 'bob'; 
+}
